1e1497cda3791b3b3cdac5908215381d72135a5f
Corporate\Suleimans <saeed.suleiman@verastar.co.uk>
2024-02-21 13:31:46 +0000

    styling and adding images to genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import useGenre from "../hooks/useGenre";
+import getCroppedImageUrl from "../services/image-url";
+
+const GenreList = () => {
+  const { data } = useGenre();
+  return (
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id}>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text>{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default GenreList;
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,10 +1,13 @@
-
-const getCroppedImageUrl = (url: string) => {
-    return url.replace('media/', 'media/crop/600/400/');
-
-    /*const target = 'media/' ;
-    const index = url.indexOf(target) + target.length;
-    return url.slice(0, index) + 'crop/600/400/' + url.slice(index);*/
-}
+const getCroppedImageUrl = (url: string): string => {
+    const target = 'media/';
+    const index = url.indexOf(target);
+    
+    if (index !== -1) {
+        return url.slice(0, index + target.length) + 'crop/600/400/' + url.slice(index + target.length);
+    } else {
+        // If 'media/' is not found in the URL, return the original URL
+        return url;
+    }
+};
 
 export default getCroppedImageUrl;
